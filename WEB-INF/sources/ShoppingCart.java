aee62f6bd209715e3cb23b1dffa07990dad17e44
Van Nguyen <vnguyen94@gmail.com>
2016-01-24 16:28:47 -0800

    create protected page (shopping cart)

---
+++ WEB-INF/sources/ShoppingCart.java
@@ -0,0 +1,30 @@
+
+/* A servlet to display the contents of the MySQL movieDB database */
+
+import java.io.*;
+import java.net.*;
+import java.sql.*;
+import java.text.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class ShoppingCart extends HttpServlet
+{
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException
+    {
+		HttpSession session = request.getSession(false);
+        PrintWriter out = response.getWriter();
+		
+		if (session == null) {
+			String redirectURL = "?redirect=" + request.getRequestURI();
+			response.sendRedirect("/login" + redirectURL);
+		} else {
+			String firstName = (String)session.getAttribute("userFirstName");
+			out.println(firstName + ", here is your shopping cart");
+		}
+		
+         out.close();
+    }
+}
