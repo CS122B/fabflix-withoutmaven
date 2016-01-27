1ffa5a8965e9aa478c8c97156af2091d0501176e
Van Nguyen <vnguyen94@gmail.com>
2016-01-26 21:36:25 -0800

    fill in movie detail modal

--- static/js/movieDetail.js
+++ static/js/movieDetail.js
@@ -1,29 +1,40 @@
 $(document).ready(function () {
 
   var $modalAddMovie = $('#modal-add-movie');
+  var $modalMovieBody = $('#modal-movie-body');
   var $buttonAddMovie = $('#button-add-movie');
   var $selectMovieQuantity = $('#movie-quantity');
+  var $movieTitle = $('#movie-title');
+  var $movieYear = $('#movie-year');
+  var $movieImage = $('#movie-image');
   var movieQuantity = $selectMovieQuantity.val();
+  var copyNum;
 
   $selectMovieQuantity.change(function (/* e */) {
     movieQuantity = $selectMovieQuantity.val();
   });
 
   $buttonAddMovie.click(function (/* e */) {
-    var movieId = $buttonAddMovie.attr('data-movie-id');
-    $.post('/TomcatForm/servlet/user/addToCart', {
-      movieId: movieId,
+    $buttonAddMovie
+      .prop('disabled', true)
+      .html('Add to Cart');
+
+    var postData = {
+      movieId: $buttonAddMovie.attr('data-movie-id'),
       quantity: movieQuantity
-    })
-    .done(function (data) {
-      console.log(data);
-      console.log('success');
-      $modalAddMovie.modal('show');
-    })
-    .fail(function (err) {
-      console.log(err);
-      alert('faillll');
-    });
+    };
+
+    $.post('/TomcatForm/servlet/user/addToCart', postData)
+      .done(function (data) {
+        copyNum = (+movieQuantity > 1) ? ' copies of ' : ' copy of ';
+        $modalMovieBody.html('You have added ' + movieQuantity + copyNum + $movieTitle.html() + ' (' + $movieYear.html() + ').');
+        $modalAddMovie.modal('show');
+      })
+      .fail(function (err) {
+        $buttonAddMovie
+          .prop('disabled', false)
+          .html('Add failed. Retry?');
+      });
   });
 
 });
