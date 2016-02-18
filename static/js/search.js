e8c2900f5b8d12c52d9adb5bd8d6219dd35cecfd
Van Nguyen <vnguyen94@gmail.com>
2016-02-17 19:58:29 -0800

    movie hover card css and js

--- static/js/search.js
+++ static/js/search.js
@@ -1,25 +1,49 @@
 $(document).ready(function () {
 
   var ROOT_PATH = '/fabflix';
+  var HOVER_INTERVAL = 2000;
+
   var $form = $('.form-horizontal .form-control');
   var $inputs = $form.filter('input');
   var $selects = $form.filter('select');
-  $('#search-reset').click(function () {
-    $inputs.val('');
-    $selects.val('none');
-  });
 
   var $modalAddMovie = $('#modal-add-movie');
   var $modalMovieMessage = $('#modal-movie-message');
 
   var $movieTitleLinks = $('.movie-title-link');
 
+  var $movieHoverCard = $('#movie-hover-card');
   var $movieHoverImage = $('#movie-hover-image');
   var $movieHoverTitle = $('#movie-hover-title');
   var $movieHoverYear = $('#movie-hover-year');
   var $movieHoverId = $('#movie-hover-id');
   var $movieHoverActors = $('#movie-hover-actors');
   var $movieHoverTrailer = $('#movie-hover-trailer');
+
+  var hoverInterval;
+  var $currentLinkHover;
+
+  function checkMovieHover() {
+    if ($currentLinkHover) {
+      var isHoveringOnCurrentLink = $currentLinkHover.is(':hover');
+      var isHoveringOnCard = $movieHoverCard.is(':hover');
+
+      if (!(isHoveringOnCurrentLink || isHoveringOnCard)) {
+        clearHoverInterval();
+      }
+    }
+  }
+
+  function clearHoverInterval() {
+      $currentLinkHover = null;
+      clearInterval(hoverInterval);
+      $movieHoverCard.hide();
+  }
+
+  $('#search-reset').click(function () {
+    $inputs.val('');
+    $selects.val('none');
+  });
 
   $('.button-add-movie').click(function (/* e */) {
     var $that = $(this);
@@ -60,31 +84,46 @@ $(document).ready(function () {
   });
 
   $movieTitleLinks.mouseenter(function (e) {
-    $.get(ROOT_PATH + '/servlet/movieHover/' + $(this).attr('data-movie-id'))
+    // clear previous interval if any
+    clearHoverInterval();
+
+    $currentLinkHover = $(this);
+    var linkPosition = $currentLinkHover.offset();
+    var hoverApiRoute = ROOT_PATH + '/servlet/movieHover/' + $currentLinkHover.attr('data-movie-id');
+
+    $.get(hoverApiRoute)
       .done(function (data) {
         var dataParsed = JSON.parse(data);
         var actors = dataParsed.actors.reduce(function (prev, curr, index) {
-          var actorLink = '<a href="' + ROOT_PATH + '/stars/' +
-            curr.id + '">' + curr.first_name + ' ' + curr.last_name + '</a>' +
-            (index === dataParsed.actors.length-1 ? '' : ', ');
-
-          return prev + actorLink;
+          return prev + (
+            '<a href="' + ROOT_PATH + '/stars/' + curr.id +
+            '">' + curr.first_name + ' ' + curr.last_name + '</a>' +
+            (index === dataParsed.actors.length-1 ? '' : ', ')
+          );
         }, '');
 
-        $movieHoverImage
-          .attr('src', dataParsed.banner_url)
-          .error(function () {
-            $('#movie-hover-image').attr('src', ROOT_PATH + '/static/images/default_poster.jpg')
-          });
+        hoverInterval = setInterval(checkMovieHover, HOVER_INTERVAL);
+
         $movieHoverTitle.text(dataParsed.title);
         $movieHoverYear.text(dataParsed.year);
         $movieHoverId.text(dataParsed.id);
         $movieHoverActors.html(actors);
         $movieHoverTrailer.attr('href', dataParsed.trailer_url);
+        $movieHoverImage
+          .attr('src', dataParsed.banner_url)
+          .error(function () {
+            $('#movie-hover-image').attr('src', ROOT_PATH + '/static/images/default_poster.jpg')
+          });
+        $movieHoverCard
+          .css({
+            top: linkPosition.top + $currentLinkHover.height(),
+            left: linkPosition.left
+          })
+          .show();
       })
       .fail(function (err) {
         console.log(err);
-      })
+      });
   });
 
 });
